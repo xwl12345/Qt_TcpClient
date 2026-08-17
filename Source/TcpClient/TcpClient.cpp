80ab8142c024a222012a05aea9ba4864f23e172b
cybot <977002146@qq.com>
2026-08-17 16:45:53 +0800

    feat：实现TcpClient部分，优化原有阻塞逻辑为异步非阻塞，新增定时器实现超时重连效果

--- Source/TcpClient/TcpClient.cpp
+++ Source/TcpClient/TcpClient.cpp
@@ -1,47 +1,82 @@
 #pragma once
 #include "TcpClient.h"
 
-TcpClient::TcpClient():m_running(false)
-					  ,m_client(nullptr)
-{
+TcpClient::TcpClient(QObject* parent):QObject(parent)
+									 ,m_client(nullptr)
+									 ,m_close(true)
+{	
+	m_Reconnect_timer = new QTimer(this);
+	m_client = new QTcpSocket(this);
+	connect(m_client, &QTcpSocket::connected, this, TcpClient::OnConnected);
+	connect(m_client, &QTcpSocket::disconnected, this, TcpClient::OnDisConnected);
+	connect(m_Reconnect_timer, &QTimer::timeout, this, TcpClient::startReconnect);
+	connect(m_client, &QTcpSocket::readyRead, this, TcpClient::OnReadyRead);
+
+
 }
 
-void TcpClient::ClientConnect(const QString& adress,const quint16 port)
+void TcpClient::connectToServer(const QString& address, const quint16 port)
 {
-	int failed=0;
-	
-	while(!m_running.load())
+	m_address = address;
+	m_port = port;
+	m_client->connectToHost(address, port);
+}
+
+void TcpClient::OnConnected()
+{
+	emit connectedSucceeded();//发送连接成功信号，在后续ui触发事件时更新信息
+
+}
+
+void TcpClient::OnReadyRead()
+{
+	QByteArray data = m_client->readAll();
+	m_parser.appendData(data);
+	while (m_parser.hasCompleteMessage())
 	{
-		if (failed == 10)
-		{
-			m_running.store(true);
-			emit requestTotalFailed();
-		}
-		if (!(m_client == nullptr))
-		{
-			m_client->abort();
-			delete m_client;
-			m_client = nullptr;
-		}
-
-		//新建一个TcpSocket
-		m_client = new QTcpSocket(this);
-		//给socket对应的端口号让他连接
-		m_client->connectToHost(adress, port);
-		//连接超时发信号
-		if (!m_client->waitForConnected(1000))
-		{
-			emit requestFailed();
-			failed += 1;
-			continue;
-		}
+		QString msg = m_parser.takeNextMessage();
+		emit MessageRecved(msg);
 	}
-
 }
 
-void TcpClient::OnConnected(const QString& data)
+void TcpClient::startReconnect()
 {
-
+	if (time > 1600)
+	{
+		emit reconnect_Timeout;
+		time = 100;
+	}
+	time *= 2;
+	OnReConnect();
+	m_Reconnect_timer->start(time);
 }
+
+
+void TcpClient::sendMessage(const QString& data)
+{
+	QByteArray message = m_parser.encode(data);
+	m_client->write(message);
+}
+
+void TcpClient::OnReConnect()
+{
+	m_client->connectToHost(m_address, m_port);
+}
+
+void TcpClient::disconnectFromServer()
+{
+	m_close.store(false);
+	m_client->abort();
+}
+
+void TcpClient::OnDisConnected()
+{
+	if(m_close.load())
+	{
+		startReconnect();//每次断连都进来启动重连定时器
+	}
+}
+
+
 
 
--- Source/TcpClient/TcpClient.h
+++ Source/TcpClient/TcpClient.h
@@ -2,24 +2,33 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <atomic>
+#include <QTimer>
 #include "ProtocolParser.h"
 class  TcpClient:public QObject
 {
     Q_OBJECT
 public:
-    TcpClient();
-    void ClientConnect(const QString& address,const quint16 port);//发送Tcp连接
-    void ClientDisconnect();//请求中断
-    void InitClient();
+    explicit TcpClient(QObject* parent=nullptr);
+    void connectToServer(const QString& address,const quint16 port);//发送Tcp连接
+    void disconnectFromServer();//请求中断
+    void sendMessage(const QString& data);
+    void OnReConnect();
 public slots:
-    void OnConnected(const QString& data);//发送数据
+    void OnConnected();
+    void OnDisConnected();
+    void OnReadyRead();
+    void startReconnect();
 signals:
-    void requestSucceeded(int data);
-    void requestFailed();
-    void finish();
-    void requestTotalFailed();
+    void connectedSucceeded();
+    void disconnectSucceeded();
+    void MessageRecved(const QString& data);
+    void reconnect_Timeout();//重连超时
 private:
     QTcpSocket* m_client;
-    std::atomic<bool> m_running;
-    std::atomic<bool> m_using;
+    ProtocolParser m_parser;
+    QString m_address;
+    quint16 m_port;
+    std::atomic<bool> m_close;//主动停止标志位
+    QTimer* m_Reconnect_timer;
+    int time;
 };
